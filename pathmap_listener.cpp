5455d6f0100f4a6f784ac007828e8d2346072c80
Tingjun Li <tingjunl@umich.edu>
2023-02-18 12:10:29 -0500

    Listener for fetch

--- pathmap_listener.cpp
+++ pathmap_listener.cpp
@@ -14,56 +14,68 @@
 #include "nav_msgs/Odometry.h"
 #include <nav_msgs/Path.h>
 
-std::string file_name_kitti = "path_kitti.txt";
-std::string file_name_tum = "path_tum.txt";
-typedef std::numeric_limits< double > dbl;
+std::string orb_slam3_file_name_tum = "orb_slam3_path_tum.txt";
+std::string curly_file_name_tum = "curly_path_tum.txt";
+typedef std::numeric_limits<double> dbl;
 
-void savePoseCallback(const geometry_msgs::Pose& pose, const long double timestamp);
+void savePoseCallback(const geometry_msgs::Pose &pose, const long double timestamp, const std::string &file_name_tum);
 
-void pathMapCallback(const nav_msgs::Path::ConstPtr& msg)
+void Orb3PathCallback(const nav_msgs::Path::ConstPtr &msg)
 {
   geometry_msgs::Pose pose;
   pose = msg->poses.back().pose;
   geometry_msgs::Point position;
   position = pose.position;
 
-  ROS_INFO("I heard: [%f, %f, %f]", position.x, position.y, position.z);
+  // ROS_INFO("I heard: [%f, %f, %f]", position.x, position.y, position.z);
   long double timestamp;
   timestamp = msg->header.stamp.sec + (double)msg->header.stamp.nsec / (double)1000000000;
-  savePoseCallback(pose, timestamp);
+  savePoseCallback(pose, timestamp, orb_slam3_file_name_tum);
 }
 
-void cameraOdomCallback(const nav_msgs::Odometry& msg)
+void CurlyPathCallback(const nav_msgs::Path::ConstPtr &msg)
+{
+  geometry_msgs::Pose pose;
+  pose = msg->poses.back().pose;
+  geometry_msgs::Point position;
+  position = pose.position;
+
+  // ROS_INFO("I heard: [%f, %f, %f]", position.x, position.y, position.z);
+  long double timestamp;
+  timestamp = msg->header.stamp.sec + (double)msg->header.stamp.nsec / (double)1000000000;
+  savePoseCallback(pose, timestamp, curly_file_name_tum);
+}
+
+void cameraOdomCallback(const nav_msgs::Odometry &msg)
 {
   geometry_msgs::Pose pose;
   pose = msg.pose.pose;
   geometry_msgs::Point position;
   position = pose.position;
 
-  ROS_INFO("I heard: [%f, %f, %f]", position.x, position.y, position.z);
+  // ROS_INFO("I heard: [%f, %f, %f]", position.x, position.y, position.z);
   long double timestamp;
   timestamp = msg.header.stamp.sec + (double)msg.header.stamp.nsec / (double)1000000000;
-  savePoseCallback(pose, timestamp);
+  savePoseCallback(pose, timestamp, curly_file_name_tum);
 }
 
-void savePoseCallback(const geometry_msgs::Pose& pose, const long double timestamp) {
-    if (file_name_kitti.size() > 0) {
-        // ROS_INFO_STREAM("write new pose\n");
-        std::ofstream outfile(file_name_kitti,std::ofstream::out | std::ofstream::app );
-        outfile << "1 0 0 "<< pose.position.x <<" 0 1 0 "<< pose.position.y <<" 0 0 1 "<< pose.position.z <<std::endl<<std::flush;
-        outfile.close();
-        // tum style
-        std::ofstream tum_outfile(file_name_tum,std::ofstream::out | std::ofstream::app );
-        // std::cout.precision(dbl::max_digits10);
-        // std::cout << timestamp << std::endl;
-        tum_outfile.precision(dbl::max_digits10);
-        tum_outfile << timestamp << " "<< pose.position.x <<" "<< pose.position.y << " "<< pose.position.z << " "<< pose.orientation.x\
-        <<" "<< pose.orientation.y <<" "<< pose.orientation.z <<" "<< pose.orientation.w <<std::endl<<std::flush;
-        
-        tum_outfile.close();
-    }
-}
+void savePoseCallback(const geometry_msgs::Pose &pose, const long double timestamp, const std::string &file_name_tum)
+{
+  if (file_name_tum.size() > 0)
+  {
+    // ROS_INFO_STREAM("write new pose\n");
+    // tum style
+    std::ofstream tum_outfile(file_name_tum, std::ofstream::out | std::ofstream::app);
+    // std::cout.precision(dbl::max_digits10);
+    // std::cout << timestamp << std::endl;
+    tum_outfile.precision(dbl::max_digits10);
+    tum_outfile << timestamp << " " << pose.position.x << " " << pose.position.y << " " << pose.position.z << " " << pose.orientation.x
+                << " " << pose.orientation.y << " " << pose.orientation.z << " " << pose.orientation.w << std::endl
+                << std::flush;
 
+    tum_outfile.close();
+  }
+}
 
 int main(int argc, char **argv)
 {
@@ -72,14 +84,16 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  std::ofstream kitti_outfile(file_name_kitti);
-  std::ofstream tum_outfile(file_name_tum);
-  kitti_outfile.close();
-  tum_outfile.close();
+  std::ofstream curly_tum_outfile(curly_file_name_tum);
+  curly_tum_outfile.close();
+  std::ofstream orb_slam3_tum_outfile(orb_slam3_file_name_tum);
+  orb_slam3_tum_outfile.close();
   std::cout << "Ready to subscribe to path_map topic" << std::endl;
-  ros::Subscriber sub = n.subscribe("zed_node/path_map", 1000, pathMapCallback);
+  ros::Subscriber sub_orb_slam3 = n.subscribe("ZED_ORBSLAM3/trajectory", 1000, Orb3PathCallback);
+  ros::Subscriber sub_curly = n.subscribe("/robot/inekf_estimation/path", 1000, CurlyPathCallback);
+
   // ros::Subscriber sub = n.subscribe("zed_node/odom", 1000, cameraOdomCallback);
-  
+
   ros::spin();
 
   return 0;
